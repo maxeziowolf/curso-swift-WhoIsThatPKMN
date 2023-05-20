bb5cd124cf6e5dfbe9dd626bf4c7c42edcedd886
maxeziowolf <maxeziowolf@outlook.com>
2023-05-20 00:33:24 -0600

    Se agrego configuraciones para la vista principal

--- who is that pokemon/PokemonViewController.swift
+++ who is that pokemon/PokemonViewController.swift
@@ -8,9 +8,33 @@
 import UIKit
 
 class PokemonViewController: UIViewController {
+    
+    //IBOutlets
+    @IBOutlet weak var labelScore: UILabel!
+    @IBOutlet weak var pokemonImage: UIImageView!
+    @IBOutlet weak var labelMessage: UILabel!
+    @IBOutlet var answerButtons: [UIButton]!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
     }
+    
+    private func setupUI(){
+        
+        //Configuracion de botones
+        for buttom in answerButtons {
+            buttom.layer.cornerRadius = 10
+            buttom.layer.masksToBounds = false
+        }
+        
+    }
+    
+    
+    @IBAction func buttomPressed(_ sender: Any) {
+        
+    }
+    
     
 }
